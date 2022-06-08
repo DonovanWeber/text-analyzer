a483c7e90aebc5fc11d606c108d54291f3cfc8ca
Donovan Weber <donovanweber03@gmail.com>
2022-06-08 11:24:55 -0400

    Add reusable business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 //Business Logic
 
 //wordCounter
-
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
@@ -10,7 +9,7 @@ function wordCounter(text) {
   const wordArray = text.split(" ");
   wordArray.forEach(function(element) {
     if (!Number(element)) {
-    wordCount++;
+      wordCount++;
     }
   });
   
@@ -20,7 +19,7 @@ function wordCounter(text) {
 //numberofOccurancesInText
 
 function numberOfOccurrencesInText(word, text) {
-  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -36,6 +35,9 @@ function numberOfOccurrencesInText(word, text) {
 //boldpassage function
 
 function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
   let htmlString = "<p>";
   let textArray = text.split(" ");
   textArray.forEach(function(element, index) {
@@ -52,6 +54,9 @@ function boldPassage(word, text) {
 }
 // UI Logic
 
+
+
+
 $(document).ready(function(){
   $("form#word-counter").submit(function(event){
     event.preventDefault();
@@ -61,33 +66,51 @@ $(document).ready(function(){
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
+    $('#bolded-passage').html(boldPassage(word, passage))
   });
 });
 
+//utility logic 
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length) === 0);
+}
 
 
-
-
-
-const word = "zoinks"
-const text = "paragraph info zoinks"
-function excludeOffensiveWords(word, text) {
-  // let wordCount = 0;
-  const wordArray = text.split(" ");
-  wordArray.forEach(function(element) {
-  let changedArray = wordArray.filter(element => element.remove('zoinks')); {
-    return changedArray;
-    
-  };
-});
-};
-     
-
-
-
-
+// const word = "zoinks"
+// const text = "paragraph info zoinks"
+// function excludeOffensiveWords(word, text) {
+//   // let wordCount = 0;
+//   const wordArray = text.split(" ");
+//   wordArray.forEach(function(element) {
+//     let changedArray = wordArray.filter(element => e ); {
+//       return changedArray;
+      
+//     };
+//   });
+// };
 
 //examples of usable functions for practice extending business logic
+
+inputText = 'hey zoinks whats loopdaloop up muppeter'
+
+function omitsBadWords(inputText, badWord) {
+  // ['hey', 'there'...]
+  const arrayOfInputText = inputText.split(" ");
+  let newArrayOfInputText = []
+  arrayOfInputText.forEach(function(text){
+    if (badWord.includes(text.toLowerCase())=== false){
+      newArrayOfInputText.push(text)
+    }
+    
+    
+  
+});
+return newArrayOfInputText.join(' ');
+};
+
+
+
+
 
 function removeBadWords(textWeWantToSearch){
   const wordArray = textWeWantToSearch.split(" "); //created array from textWeWantToSearch
